8c2dcf6e3bc1cc00656b2792c408fa3e935f872a
Pacôm Charbel YABI <64853593+SteelHeart@users.noreply.github.com>
2022-06-08 15:40:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, nemo. At, fuga rerum corrupti itaque cum ipsa nemo possimus eius vitae culpa nobis, hic explicabo inventore pariatur laboriosam. Explicabo, laudantium?'));
+app.get('/', (req, res) => res.send('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, nemo. At, fuga rerum corrupti itaque cum ipsa nemo possimus eius vitae culpa nobis, hic explicabo inventore pariatur laboriosam. Explicabo, laudantium? <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, nemo. At, fuga rerum corrupti itaque cum ipsa nemo possimus eius vitae culpa nobis, hic explicabo inventore pariatur laboriosam. Explicabo, laudantium?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
